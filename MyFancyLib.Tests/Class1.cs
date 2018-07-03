4ac90b897ce1c933292a7b17255acddd4e0d8d94
Dominik Rauch <dominik@rauch.io>
2018-07-03 13:34:08 +0200

    Add MyFancyLib and MyFancyLib.Tests projects.

---
+++ MyFancyLib.Tests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFancyLib.Tests
+{
+    public class Class1Tests
+    {
+      public void MyTest()
+      {
+        var class1 = new Class1();
+      }
+    }
+}
