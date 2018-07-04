b41d980fd2a8976d0a2ba6e06c648f907d916f72
Dominik Rauch <dominik@rauch.io>
2018-07-04 08:55:39 +0200

    Switch to .NET Standard projects.

---
+++ GoatLib.Tests/ClassTest.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace GoatLib.Tests
+{
+  [TestFixture]
+  public class ClassTest
+  {
+    [Test]
+    public void Test()
+    {
+      var @class = new Class();
+      @class.Prop.Should().Be("Greatest Of All Time");
+    }
+  }
+}
---
+++ GoatLib/Class.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace GoatLib
+{
+  public class Class
+  {
+    public string Prop => "Greatest Of All Time";
+  }
+}
